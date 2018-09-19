bbc6a0241b588b4dd80f7e04e7ae41646abaa9ea
andrewjmcgehee <andrewjmcgehee.us@gmail.com>
2018-09-19 02:25:39 -0500

    update solutions in next several commits

---
+++ Python/10kinds.py
@@ -0,0 +1,48 @@
+# Rating: ~ 5.5 / 10
+# Link: https://open.kattis.com/problems/10kindsofpeople
+# Complexity: O(N log(N)) due to union find and where N is number of cells in row-major form
+# Memory: O(Rows * Columns) for union find in row-major form
+
+
+# light weight union find representation
+def unite(uf, x, y):
+  uf[x] = find(uf, y)
+
+def find(uf, x):
+  if uf[x] == x:
+    return x
+  else:
+    uf[x] = find(uf[x])
+  return uf[x]
+
+
+row, col = map(int, input().split())
+uf = [i for i in range(row * col)]
+board = []
+
+# build board
+for i in range(row):
+  board.append(input())
+
+# unite all elements of same type
+for i in range(row):
+  for j in range(col):
+    if j < col - 1 and board[i][j] == board[i][j+1]:
+      unite(uf, i*col + j, i*col + j+1)
+    if i < row - 1 and board[i][j] == board[i+1][j]:
+      unite(uf, i*col + j, (i+1)*col + j)
+
+# queries have a path between them if they return the same representative
+# element
+
+m = int(input())
+for _ in range(m):
+  r1, c1, r2, c2 = [int(x)-1 for x in input().split()]
+
+  if find(uf, r1*col + c1) == find(uf, r2*col + c2):
+    if board[r1][c1] == '1':
+      print('decimal')
+    else:
+      print('binary')
+  else:
+    print('neither')
---
+++ Python/kastenlauf.py
@@ -0,0 +1,61 @@
+# Rating: ~ 3.7 / 10
+# Link: https://open.kattis.com/problems/kastenlauf
+# Complexity: O(N log(N)) due to union find and where N is number of stops
+# Memory: O(N) where N is number of stops
+
+class UnionFind:
+  def __init__(self, n):
+    self.parent = [i for i in range(n)]
+
+  def union(self, x, y):
+    self.parent[self.find(x)] = self.find(y)
+
+  def find(self, x):
+    if self.parent[x] == x:
+      return x
+    else:
+      self.parent[x] = self.find(self.parent[x])
+      return self.parent[x]
+
+
+def main():
+  n = int(input())
+  for _ in range(n):
+    m = int(input())
+
+    # source
+    x, y = (int(x) for x in input().split())
+    stops = [(x, y)]
+
+    # stores
+    for i in range(m):
+      x, y = (int(x) for x in input().split())
+      stops.append((x, y))
+
+    # target
+    x, y = [int(x) for x in input().split()]
+    stops.append((x, y))
+
+    # note that source is at index 0 and target is at index -1
+
+    # 20 bottles of beer, 1 bottle every 50 meters gives that no distance
+    # can be > 1000 meters
+
+    # simply consider all pairwise relationships and union if less than 1000 meters
+    # apart by manhattan distance
+    uf = UnionFind(len(stops))
+    for i in range(len(stops)):
+      for j in range(i+1, len(stops)):
+        a = stops[i]
+        b = stops[j]
+        if abs(a[0] - b[0]) + abs(a[1] - b[1]) <= 1000:
+          uf.union(i, j)
+
+    if uf.find(0) == uf.find(len(stops)-1):
+      print("happy")
+    else:
+      print("sad")
+
+
+if __name__ == '__main__':
+  main()
