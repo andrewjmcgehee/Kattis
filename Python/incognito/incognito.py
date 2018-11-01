1358a7d84334f8eb599eb3e71af47176864acd7a
andrewjmcgehee <andrewjmcgehee.us@gmail.com>
2018-10-31 21:05:00 -0500

    update solutions in next several commits

--- Python/incognito/incognito.py
+++ Python/incognito/incognito.py
@@ -1,24 +1,30 @@
+# Rating: ~ 2.9
+# Link: https://open.kattis.com/problems/incognito
+# Complexity: O(N) for N articles of clothing
+# Memory: O(N) for N articles of clothing
+
 def main():
   t = int(input())
   for _ in range(t):
     n = int(input())
 
+    # stored types of clothing
     categories = dict()
-    types = set()
-
+    # add all clothing to map
     for i in range(n):
       piece, cat = input().split()
       if cat not in categories:
+        # only track distinct items
         categories[cat] = set()
-        types.add(cat)
       categories[cat].add(piece)
-
+    # initial 1 so we can multiply
     num = 1
-    for i in types:
-      num *= (len(categories[i])+1)
+    for k, v in categories:
+      # add 1 to length to signify not wearing that item
+      num *= (len(v) + 1)
+    # subtract 1 from earlier
     num -= 1
     print(num)
-
 
 if __name__ == '__main__':
   main()
--- Python/keytocrypto/keytocrypto.py
+++ Python/keytocrypto/keytocrypto.py
@@ -1,42 +1,28 @@
-from sys import stdin, stdout
+# Rating: ~ 1.7 / 10
+# Link: https://open.kattis.com/problems/keytocrypto
+# Complexity: O(
+# Memory: O(
+
 
 def main():
-    SHIFTS = {'A':0, 'B':1, 'C':2, 'D':3,
-              'E':4, 'F':5, 'G':6, 'H':7,
-              'I':8, 'J':9, 'K':10, 'L':11,
-              'M':12, 'N':13, 'O':14, 'P':15,
-              'Q':16, 'R':17, 'S':18, 'T':19,
-              'U':20, 'V':21, 'W':22, 'X':23,
-              'Y':24, 'Z':25}
+  # maps character to integer
+  shifts = { chr(i+65):i for i in range(26) }
+  cipher = input()
+  # will progressively append to end
+  keytxt = list(input())
 
-    cipher = stdin.readline().strip()
-    keytxt = stdin.readline().strip()
-    translation = []
-    current_index = 0
-    result = ''
-
-    for i in range(len(keytxt)):
-        translation.append(SHIFTS[keytxt[i]])
-        current_index += 1
-
-    i = 0
-    while True:
-        if len(result) == len(cipher):
-            break
-        else:
-            if SHIFTS[cipher[i]] - translation[i] >= 0:
-                result += chr(ord(cipher[i]) - translation[i])
-            else: 
-                result += chr(ord(cipher[i]) - translation[i] + 26)
-            translation.append(SHIFTS[result[i]])
-            i += 1
-
-    print(result)
-        
-    
-    
+  # will join at end
+  ans = []
+  for i in range(len(cipher)):
+    # shift char
+    res = ord(cipher[i]) - shifts[keytxt[i]]
+    # check shifted to far
+    if res < ord('A'):
+      res += 26
+    # add to key and to answer
+    keytxt.append(chr(res))
+    ans.append(chr(res))
+  print(''.join(ans))
 
 if __name__ == '__main__':
     main()
-
-
