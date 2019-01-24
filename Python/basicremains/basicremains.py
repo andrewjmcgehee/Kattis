9553194a3b3ce0acb424494fdc0a5a2ae29e7b32
andrewjmcgehee <andrewjmcgehee.us@gmail.com>
2019-01-24 14:31:34 -0600

    annotate basicremains

--- Python/basicremains/basicremains.py
+++ Python/basicremains/basicremains.py
@@ -1,8 +1,9 @@
 # Rating: ~ 3.4 / 10
 # Link: https://open.kattis.com/problems/basicremains
+# Complexity: O(N) for N characters in digit
+# Memory: O(1)
 
-BASES = "0123456789"
-
+# helper to convert base 10 to any base
 def to_base(x, b):
   res = ""
   while x:
@@ -16,10 +17,12 @@ def main():
   while True:
     line = input().split()
     base = int(line[0])
+    # end of input
     if base == 0:
       break
     x, y = line[1], line[2]
 
+    # already optimized, dont re-implement the wheel
     a = int(x, base)
     b = int(y, base)
 
