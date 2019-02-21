3cd51a7a2d7f510404e6378d658baf47e986e8cc
andrewjmcgehee <andrewjmcgehee.us@gmail.com>
2019-02-20 18:10:06 -0600

    update solution for reachableroads

--- Python/reachableroads/reachableroads.py
+++ Python/reachableroads/reachableroads.py
@@ -3,43 +3,33 @@
 # Complexity: O(N log N) for union find
 # Memory: O(N) for N cities
 
-# classic union find problem
-class UnionFind:
-  def __init__(self):
-    self.parent = [i for i in range(1005)]
+def union(arr, x, y):
+  x_root = find(arr, x)
+  y_root = find(arr, y)
+  arr[y_root] = x_root
 
-  def union(self, x, y):
-    self.parent[self.find(y)] = self.find(x)
-
-  def find(self, x):
-    if self.parent[x] == x:
-      return x
-    # path compression done iteratively for recursion depth RTE
-    indices = []
-    while self.parent[x] != x:
-      indices.append(x)
-      x = self.parent[x]
-    while indices:
-      i = indices.pop()
-      self.parent[i] = x
+def find(arr, x):
+  if arr[x] == x:
     return x
+  arr[x] = find(arr, arr[x])
+  return arr[x]
 
 def main():
   n = int(input())
   for i in range(n):
     m = int(input())
-    uf = UnionFind()
+    arr = [i for i in range(m)]
     r = int(input())
     for j in range(r):
       # union roads
       a, b = map(int, input().split())
-      uf.union(a, b)
+      union(arr, a, b)
 
     # number of connected components
     comps = set()
     for j in range(m):
       # finding compresses path
-      comps.add(uf.find(j))
+      comps.add(find(arr, j))
     # for n cities, there are n-1 roads
     print(len(comps)-1)
 
