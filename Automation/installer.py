254ee42d6ae51279fe74b11c8379ec79811c90f7
andrewjmcgehee <andrewjmcgehee.us@gmail.com>
2019-03-24 03:00:35 -0500

    update installer

--- Automation/installer.py
+++ Automation/installer.py
@@ -5,16 +5,16 @@ parser = argparse.Parser()
 parser.add_argument("--zsh", help="install zsh completions", action="store_true")
 args = parser.parse_args()
 
-os.system("mv -v katti.py /usr/local/opt")
+os.system("cp -v katti.py /usr/local/opt")
 os.system("echo 'python3 /usr/local/opt/katti.py \"$@\"' > /usr/local/bin/katti")
 os.system("chmod +x /usr/local/bin/katti")
 os.system("mkdir -v /usr/local/etc/katti")
-os.system("mv -v problem_ids.json /usr/local/etc/katti")
+os.system("cp -v problem_ids.json /usr/local/etc/katti")
 os.system("pip3 install -r requirements.txt")
 
 if args.zsh:
   os.system("mkdir -v $HOME/.zsh-completions")
-  os.system("mv -v _katti $HOME/.zsh-completions")
+  os.system("cp -v _katti $HOME/.zsh-completions")
   os.system("echo 'fpath=($HOME/.zsh-completions $fpath)' >> $HOME/.zshrc")
   os.system("exec zsh")
 
