0fda7d934fd07c48de64c2b8ff83acd0a49c6f3b
Andrew McGehee <andrewjmcgehee.us@gmail.com>
2020-01-03 19:36:02 -0600

    solve

---
+++ peasoup/peasoup.py
@@ -0,0 +1,19 @@
+# peasoup
+# 2.3
+
+def main():
+  n = int(input())
+  for i in range(n):
+    k = int(input())
+    name = input()
+    items = set()
+    for j in range(k):
+      items.add(input())
+    if 'pea soup' in items and 'pancakes' in items:
+      print(name)
+      break
+  else:
+    print('Anywhere is fine I guess')
+
+if __name__ == '__main__':
+  main()
